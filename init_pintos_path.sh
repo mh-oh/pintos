e3ff8f1893600b45280eb7d97b8edcf636392190
Minhyeon Oh <minhyeon.oh@gmail.com>
2020-09-19 00:13:08 +0900

    Fix for running in qemu. (after clone, run ./init_pintos_path.sh)

---
+++ init_pintos_path.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+bashrc=~/.bashrc
+
+baktime=$(date +%Y.%m.%d.%H:%M:%S)
+cp $bashrc $bashrc.bak.$baktime
+echo "backup file of $bashrc is created. ($baktime)"
+
+# if defined, remove
+if grep -q PINTOS_ROOT $bashrc; then
+    grep -v "PINTOS_ROOT" $bashrc > tmp && mv tmp $bashrc
+fi
+
+# add environment vars
+cd ../
+line="export PINTOS_ROOT=$PWD/pintos\nexport PATH=\$PINTOS_ROOT/src/utils:\$PATH"
+echo "$line" | sudo tee -a $bashrc
