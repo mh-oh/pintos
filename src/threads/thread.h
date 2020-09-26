722eeba05909804e3cce7bb4d11245648026b992
Minhyeon Oh <minhyeon.oh@gmail.com>
2020-09-26 21:21:26 +0900

    Add initialization of sleep_list. (Minor error solved.)
    Core implementations for sleep/wakeup are moved into thread.c.
    
    Passed:
      - make tests/threads/alarm-single.result
      - make tests/threads/alarm-multiple.result
      - make tests/threads/alarm-simultaneous.result
      - make tests/threads/alarm-zero.result
    
    While testing the mechanisms, some error has been resolved.
    This produced an error: (e is changed after list_remove)
      - e = list_remove (e);
      - thread_unblock (list_entry (e, struct thread, elem));

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -129,6 +129,9 @@ const char *thread_name (void);
 void thread_exit (void) NO_RETURN;
 void thread_yield (void);
 
+void thread_sleep (int64_t wakeup_ticks);
+void thread_wakeup (int64_t ticks);
+
 /* Performs some operation on thread t, given auxiliary data AUX. */
 typedef void thread_action_func (struct thread *t, void *aux);
 void thread_foreach (thread_action_func *, void *);
